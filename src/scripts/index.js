b5c692201109cc2b2b7aa77bb0c2b10ce4dac1ac
yeffasol <yeffasol@mail.ru>
2016-12-27 07:52:55 +0300

    Add ajax for pages

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,23 +8,41 @@
     }
   });
 
-  (function () {
+  // (function () {
+  //
+  //   var el = document.querySelectorAll('.menu__item a');
+  //   var url = window.location.href;
+  //
+  //   for (let i = 0; i < el.length; i++) {
+  //     if ((window.location.href.substr(window.location.href.lastIndexOf("/") + 1)) == "") {
+  //       var activeItem = document.querySelector('.menu__item--portfolio');
+  //       activeItem.classList.add('menu__item--active');
+  //
+  //     }
+  //     else if (url == el[i].href) {
+  //
+  //       el[i].parentNode.classList.add('menu__item--active');
+  //
+  //     }
+  //   }
+  //
+  // })();
 
-    var el = document.querySelectorAll('.menu__item a');
-    var url = window.location.href;
-
-    for (let i = 0; i < el.length; i++) {
-      if ((window.location.href.substr(window.location.href.lastIndexOf("/") + 1)) == "") {
-        var activeItem = document.querySelector('.menu__item--portfolio');
-        activeItem.classList.add('menu__item--active');
-
-      }
-      else if (url == el[i].href) {
-
-        el[i].parentNode.classList.add('menu__item--active');
-
-      }
+  $(document).ready(function(){
+    let link = $('.menu a');
+    if ((window.location.href.substr(window.location.href.lastIndexOf("/") + 1)) == "") {
+      let activeItem = $('.menu__item--portfolio a');
+      let src= activeItem.attr('href');
+      let url = src.substr(src.lastIndexOf("/") + 1);
+      $('.inner-content').load(url);
+      activeItem.parent().addClass('menu__item--active');
     }
-
-  })();
-
+    link.click(function(){
+     link.parent().removeClass('menu__item--active');
+      let src= $(this).attr('href');
+      let url = src.substr(src.lastIndexOf("/") + 1);
+      $('.inner-content').load(url);
+      $(this).parent().addClass('menu__item--active');
+      return false;
+    })
+  });
